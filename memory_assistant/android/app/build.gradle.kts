97eeffa1e66d6e25a71178ea07328be09321de18
saksham-singhpawar <sakshamsinghpawar3@gmail.com>
2025-06-13 19:48:19 +0530

    Day 1 code working

--- memory_assistant/android/app/build.gradle.kts
+++ memory_assistant/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "com.example.memory_assistant"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
